a19998ad2c6b8281f2bfcbf1d7dc4bd23d6550d4
esther-ff <creepermaster000@gmail.com>
2025-07-03 01:39:10 +0200

    ideas live forever

--- modules/system.nix
+++ modules/system.nix
@@ -27,10 +27,51 @@ in {
       default = "x86_64-linux";
     };
 
+    # it seems silly to assign pronouns to this mere machine
+    # but think how much you interact with them
+    # don't they resemble somewhat of a person
+    # with their error codes, error messages and delightful successes
+    # they're somewhat of a gateway to another world
+    # how would i meet the person mentioned below
+    # with this marvel of engineering
+    # but also that comes at a cost
+    # it is *privilege* to use this machine currently
+    # think of the rest of the earth with no access to it
+    # or very limited one at best
+    # it is not only a tool of entertainment
+    # but also of a new age of bypassing governments
+    # where in 1878 a newspaper could get simply closed down
+    #
+    # as an example 
+    # the co-operative restaurant founded
+    # partially by Eugene Varlin was closed in 1871
+    # due to governmental repression 
+    # following the fall of the Paris Commune
+    # (Mr. Varlin himself was shot on the 28th of May)
+    #
+    # the liberty of the world depends on those devices
+    # being free from corporate and government reach
+    #
+    # before dismissing this as a funny note because i'm trans and
+    # ha ha! over-insertion of the self and the unique.
+    #
+    # think of what this comment describes more clearly
+    #
+    # now may i, the writer of this and this Nix function itself
+    # say goodbye to you, dear reader.
     pronouns = mkOption {
+      # treat humanely those who are oppressed
+      # destroy the oppressors
+      # 
+      # the average person has
+      # more in common with the homeless person
+      # on the streat
+      # than with the billionaire CEO
+      #
+      # :3
       description = "pronouns of this machine, treat them humanely too :3";
       type = types.listOf types.str;
-      default = [ "she" "they" ];
+      default = null; # be free, define yourself, not constrained
     };
 
     # i dedicate this line to poz
